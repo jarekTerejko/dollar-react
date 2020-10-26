9790969662f09c4d50000cd864c538fc9d48270f
jarekTerejko <j.terejko@gmail.com>
2020-10-26 05:39:46 +0100

    add 2px border

--- src/components/ButtonElement.js
+++ src/components/ButtonElement.js
@@ -9,6 +9,8 @@ export const Button = styled(LinkScroll)`
   font-size: ${({ fontBig }) => (fontBig ? "20px" : "16px")};
   outline: none;
   border: none;
+  border: ${({ primary }) =>
+    primary ? "2px solid #cd6133" : "2px solid #010606"};
   cursor: pointer;
   white-space: nowrap;
   border-radius: 50px;
@@ -16,10 +18,12 @@ export const Button = styled(LinkScroll)`
   text-decoration: none;
   align-items: center;
   justify-content: center;
+  font-weight: 700;
+  letter-spacing: 1px;
   transition: 0.3s;
 
   &:hover {
     background: ${({ primary }) => (primary ? "#fff" : "#cd6133;")};
-    color: #010606;
+    color: #cd6133;
   }
 `;
